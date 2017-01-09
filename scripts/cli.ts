5add0b00ca0d6c05773af57d3d3a4cd3603324e4
John Reitano <jreitano@gmail.com>
2017-01-09 12:25:17 -0800

    save work

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -4,6 +4,7 @@
 
 import * as firebase from 'firebase';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 var log = {
   info: console.log,
@@ -36,9 +37,21 @@ let userRef = (id) => {
 
 let getUserStatus;
 getUserStatus = (user) => {
-  let status = _.trim((user.registration && user.registration.status) || '') || 'initial';
-  if (status === 'initial' && user.wallet && user.wallet.address) {
-    status = 'wallet-generated';
+  let status = _.trim((user.registration && user.registration.status) || '');
+  if (status !== 'announcement-confirmed' && status != 'announcement-initiated')  {
+    if (user.wallet && user.wallet.address) {
+     let sponsor = users[user.sponsor && user.sponsor.userId];
+      if (user.disabled) {
+        status = 'disabled';
+      } else if (!sponsor || sponsor.disabled || !sponsor.wallet ||
+        !sponsor.wallet.announcementTransaction || !sponsor.wallet.announcementTransaction.blockNumber) {
+        status = 'waiting-on-upline';
+      } else {
+        status = 'ready-to-confirm';
+      }
+    } else {
+      status = 'needs-wallet';
+    }
   }
   return status;
 }
@@ -198,6 +211,8 @@ scenario6 = (u: any): boolean => {
   return u.d.scenario6;
 }
 
+let Web3 = require('web3');
+let web3 = new Web3(new Web3.providers.HttpProvider('https://www.relay.ur.technology:9596'));
 
 usersRef().once('value', (snapshot) => {
   users = snapshot.val() || {};
@@ -205,6 +220,373 @@ usersRef().once('value', (snapshot) => {
   log.info(`${_.size(users)} users loaded`);
   log.info('user groups', userGroups());
 });
+
+
+let orphans = _.filter(users, (u) => { return u.sponsor && u.sponsor.userId === unknown.userId; });
+
+let fixOrphans = () => {
+  _.each(orphans, (o) => {
+    // let sponsorName = o.prefineryUser && o.prefineryUser.userReportedReferrer.toLowerCase();
+    // let sponsors = _.filter(users, (x) => { return x.name.toLowerCase() === sponsorName; });
+    //
+    // if (_.size(sponsors) !== 1 || sponsors[0].userId === o.userId) {
+    //   return undefined;
+    // }
+    let sponsor = eiland;
+    let sponsorInfo = _.pick(sponsor, ['name', 'profilePhotoUrl', 'userId']);
+    sponsorInfo.announcementTransactionConfirmed = !!sponsor.wallet &&
+      !!sponsor.wallet.announcementTransaction &&
+      !!sponsor.wallet.announcementTransaction.blockNumber &&
+      !!sponsor.wallet.announcementTransaction.hash;
+    userRef(o.userId).update({sponsor: sponsorInfo});
+  });
+};
+fixOrphans();
+
+
+
+let orphans = _.filter(users, (u) => { return u.sponsor && u.sponsor.userId === unknown.userId; });
+let fixOrphans = () => {
+  _.each(orphans, (o) => {
+    let reportedSponsorName = o.prefineryUser && o.prefineryUser.userReportedReferrer;
+    let reportedSponsorLastName = _.last(reportedSponsorName.split(" "));
+    let sponsors = _.filter(users, (x) => {
+      return x.name && x.name.toLowerCase() === reportedSponsorName.toLowerCase() && x.userId !== o.userId;
+    });
+    if (_.size(sponsors) === 1) {
+      log.info(`${o.email} - ${o.name} sponsored by ${ sponsors[0].name} ?`);
+    }
+  });
+};
+fixOrphans();
+
+
+let bigBlockers = () => {
+  let blockers = _.sortBy(
+    _.filter(users, (u) => {
+      return getUserStatus(u) === 'needs-wallet' && u.downlineSize > 0;
+    }),
+    'downlineSize'
+  );
+  return _.map(_.takeRight(blockers, 20), (u) => {
+    return `${u.email || u.name} is blocking ${u.downlineSize} users`
+  }).join('\n');
+}
+bigBlockers();
+
+let blocker = (u) => {
+  let sponsor = users[u.sponsor && u.sponsor.userId];
+  if (!u.wallet || !u.wallet.address || u.disabled || !sponsor) {
+    return u;
+  }
+  if (sponsor.email === 'eiland@ur.technology') {
+    return undefined;
+  }
+  return blocker(sponsor);
+};
+
+let showFDBlockers = () => {
+  let emails = [
+    "jojovallejo@gmail.com",
+    "doerbank80@gmail.com",
+    "nnamanichima@gmail.com",
+    "xtianbitcoin@gmail.com",
+    "fajardoperez1965@gmail.com",
+    "lawalbasit89@gmail.com",
+    "jazzua@yandex.ru",
+    "safiullahkhankhalil@gmail.com",
+    "jr1802@yahoo.com.au",
+    "eberenc@gmail.com",
+    "nguyendinhdung141294@gmail.com",
+    "roberttrantnmu@gmail.com",
+    "baligning@gmail.com",
+    "prabhatsinha281@gmail.com"
+  ];
+  return _.map(emails, (email) => {
+    let u = _.find(users, {email: email});
+    let b = blocker(u);
+    return `${email} is blocked by ${b ? (b === u ? 'self' : (b.email || b.name)) : 'no one'}`;
+  }).join("\n");
+}
+showFDBlockers();
+
+
+
+
+let userValues = _.values(users);
+let stop = false;
+let refs = [];
+let batchSize = 500;
+let deleteDuplicateEvents = (startingUserIndex) => {
+  if (stop || startingUserIndex >= _.size(userValues)) { log.info('exiting'); return; }
+  let slice = _.slice(userValues,startingUserIndex,startingUserIndex+batchSize);
+  _.each(slice, (u) => {
+    if (stop) { log.info('exiting'); return; }
+    let events = u.events || {};
+    _.each(events, (e,eid) => { e.eventId = eid; });
+    events = _.filter(events, { sourceType: 'transaction' });
+    let g = _.groupBy(events, 'sourceId');
+    _.each(g, (eventGroup, transactionId) => {
+      if (stop) { log.info('exiting'); return; }
+      if (eventGroup.length > 1) {
+        _.each(_.slice(eventGroup, 1), (e) => {
+          if (stop) { log.info('exiting'); return; }
+          refs.push(userRef(u.userId).child(`events/${e.eventId}`).toString());
+        });
+      };
+    });
+  });
+  log.info("waiting 3 second...")
+  _.delay(deleteDuplicateEvents, 500, startingUserIndex + batchSize);
+}
+deleteDuplicateEvents(0);
+
+
+
+let transactionIds = _.uniq(_.flatten(_.map(users, (u) => {
+  return _.keys(u.transactions || {});
+})));
+
+let stop = false;
+let badTransactionIds = [];
+let batchSize = 50;
+let findBadTransactionIds = (startingIndex) => {
+  let slice = _.slice(transactionIds,startingIndex,startingIndex+batchSize);
+  if (stop || _.size(slice) === 0) { log.info('exiting'); return; }
+  let newBadTransactionIds = _.reject(slice, (tid) => {
+    return web3.eth.getTransaction(tid);
+  });
+  badTransactionIds = badTransactionIds.concat(newBadTransactionIds);
+  log.info(`found ${_.size(newBadTransactionIds)} bad transaction ids`);
+  log.info("waiting 2 seconds...")
+  _.delay(findBadTransactionIds, 2000, startingIndex + batchSize);
+}
+findBadTransactionIds(3200);
+_.size(badTransactionIds);
+
+let str = `send email, sender user id,receiver address, ur amount\n`;
+_.each(users, (u) => {
+  let badTransactionsForThisUser = _.pick(u.transactions || {}, badTransactionIds);
+  _.each(badTransactionsForThisUser, (tx, txid) => {
+    let urAmount = parseInt(tx.amount,16) / 1000000000000000000;
+    str = str + `${u.email}, ${u.userId}, ${tx.urTransaction.to}, ${urAmount}\n`
+    let ref = userRef(u.userId).child(`transactions/${txid}`);
+    ref.remove();
+  });
+});
+log.info(str);
+
+let stop = false;
+let userValues = _.filter(users, 'wallet.announcementTransaction.blockNumber');
+userValues = _.filter(userValues, 'wallet.announcementTransaction.hash');
+let badWalletUsers = [];
+let batchSize = 25;
+let findBadWalletUsers;
+findBadWalletUsers = (startingIndex) => {
+  let slice = _.slice(userValues,startingIndex,startingIndex+batchSize);
+  if (stop || _.size(slice) === 0) { log.info('exiting'); return; }
+  let newBadWalletUsers = _.filter(slice, (u) => {
+    if (stop) { return false; }
+    let userTransaction = (u.transactions || {})[u.wallet.announcementTransaction.hash];
+    if (!userTransaction || !userTransaction.urTransaction || userTransaction.urTransaction.blockNumber !== u.wallet.announcementTransaction.blockNumber) {
+      log.info(`no matching transaction for user ${u.userId}`);
+      return true;
+    }
+    // let b = web3.eth.getBalance(u.wallet.address).toNumber();;
+    // if (!b) {
+    //   log.info(`got zero balance for user ${u.userId}`);
+    //   return true;
+    // }
+    return false;
+  });
+  badWalletUsers = badWalletUsers.concat(newBadWalletUsers);
+  log.info(`found ${_.size(newBadWalletUsers)} in wallets ${startingIndex} to ${startingIndex + batchSize - 1}...`);
+  _.delay(findBadWalletUsers, 2000, startingIndex + batchSize);
+}
+findBadWalletUsers(625);
+_.size(badWalletUsers);
+
+_.size(userValues)
+
+log.info(`deleteCount=${_.size(refs)}`);
+stop=true
+
+let now;
+now = new Date().valueOf();
+
+let userEvents = [];
+let userTransactions = [];
+_.each(users, (u) => {
+  let transactions = u.transactions || {};
+  let events = u.events || {};
+  _.each(events, (e, eventId) => {
+    if (e.sourceType === 'transaction') {
+      let t = transactions[e.sourceId];
+      if (!t) {
+        throw `could not find transaction ${e.sourceId} from event ${eventId} for user ${u.userId}`;
+      }
+      if (t.urTransaction && t.urTransaction.blockNumber >= 444000) {
+        userEvents.push({userId: u.userId, eventId: eventId});
+      }
+    }
+  });
+  _.each(transactions, (t, transactionId) => {
+    if (t.urTransaction && t.urTransaction.blockNumber >= 444000) {
+      userTransactions.push({userId: u.userId, transactionId: transactionId});
+    }
+  });
+});
+log.info(`_.size(userEvents)=${_.size(userEvents)}`);
+log.info(`_.size(userTransactions)=${_.size(userTransactions)}`);
+
+_.each(userEvents, (userEvent) => {
+  let ref = firebase.database().ref(`/users/${userEvent.userId}/events/${userEvent.eventId}`);
+  ref.remove();
+});
+_.each(userTransactions, (userTransaction) => {
+  let ref = firebase.database().ref(`/users/${userTransaction.userId}/transactions/${userTransaction.transactionId}`);
+  ref.remove();
+});
+
+
+let updateCurrentBalance;
+let Web3;
+let web3;
+let count;
+let needsBalance;
+let usersNeedingBalance;
+
+
+
+count = 0;
+updateCurrentBalance = (u: any) => {
+  if (!u.userId) {
+    throw 'no userId found';
+  }
+
+  if (!u.wallet || !u.wallet.address) {
+    throw `no user address found`;
+  }
+
+  if (u.wallet.currentBalance) {
+    return;
+  }
+
+  let currentBalance: BigNumber;
+  try {
+    currentBalance = web3.eth.getBalance(u.wallet.address);
+  } catch(error) {
+    throw `got error when attempting to get balance for address ${u.wallet.address} and user ${u.userId}`;
+  }
+
+  userRef(u.userId).child('wallet').update({currentBalance: currentBalance.toFixed()}).then(() => {
+    u.wallet.currentBalance = currentBalance.toFixed();
+    // log.info(`updated user ${u.userId} to ${currentBalance.toFormat(0)}`);
+    log.info(` updated balance for user ${count}`);
+    count++;
+
+  }, (error: any) => {
+    thow `could not update current balance for user ${u.userId}: ${error}`;
+  });
+
+}
+
+// let john = _.find(users, {email: 'jreitano@ur.technology'});
+// updateCurrentBalance(john);
+
+let t1, t2, batchSize;
+let remainingItems;
+
+needsBalance = (u) => { return !!u.wallet && !!u.wallet.announcementTransaction && !!u.wallet.announcementTransaction.blockNumber && !u.wallet.currentBalance; };
+
+
+usersNeedingBalance = _.filter(users, needsBalance);
+log.info(`${_.size(usersNeedingBalance)} still need balance`);
+batchSize = 700;
+_.each(_.slice(usersNeedingBalance,0,batchSize), updateCurrentBalance);
+
+
+
+
+let Web3 = require('web3');
+let web3 = new Web3(new Web3.providers.HttpProvider('https://relay.ur.technology:9596'));
+let filter = web3.eth.filter({fromBlock: 0, toBlock: 'latest', address: '0x482cf297b08d4523c97ec3a54e80d2d07acd76fa'});
+let filterResults = filter.get((error, logEntries) => {
+  _.each(logEntries, (logEntry) => {
+    log.info(logEntry);
+  });
+});
+
+filterResults = filter.get(function(error, logEntries) {
+  logEntries.forEach(function(logEntry) {
+    log.info(logEntry);
+  });
+});
+
+log.info(filterResults);
+
+
+
+
+
+
+
+function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber) {
+  if (endBlockNumber == null) {
+    endBlockNumber = eth.blockNumber;
+  }
+  if (startBlockNumber == null) {
+    startBlockNumber = endBlockNumber - 1000;
+  }
+
+  var transactions = [];
+
+  for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+    if (i % 1000000 == 1) {
+      console.log("Searching block " + i);
+    }
+    var block = eth.getBlock(i, true);
+    if (block != null && block.transactions != null) {
+      block.transactions.forEach( function(e) {
+        if (myaccount == "*" || myaccount == e.from || myaccount == e.to) {
+          transactions.push(e);
+        }
+      })
+    }
+  }
+
+  console.log('loaded', transactions.length, 'transactions');
+}
+
+let growth;
+growth = (a, b) => {
+  let now = new Date().valueOf();
+  let sample = _.filter(users, (u) => {
+    let hoursAgoCreated = (now - u.createdAt)/60/60/1000;
+    return a < hoursAgoCreated && hoursAgoCreated <= b && u.wallet && u.wallet.announcementTransaction && u.wallet.announcementTransaction.blockNumber;
+  });
+  log.info(`size=${_.size(sample)}`);
+  let nextGen = _.filter(users, (u) => {
+    return u.sponsor && _.some(sample, (s) => { return s.userId === u.sponsor.userId; });
+  });
+  log.info(`sample size=${_.size(sample)}, next gen size=${_.size(nextGen)}, growth rate=${(_.size(nextGen) - _.size(sample))/ _.size(sample) * 100}`);
+};
+growth(12, 24);
+
+
+
+let x, y;
+x = _.groupBy(users, 'phone');
+
+y = _.filter(x, (group, phone) => {
+  if (!phone || group.length === 1 || group.length > 2000) {
+    return false;
+  }
+  let haveWallets = _.filter(group, 'wallet.announcementTransaction');
+  return haveWallets.length > 1;
+});
+
+
 
 // step 2
 
@@ -256,7 +638,7 @@ _.groupBy(_.values(users), description);
 let announceUsers;
 announceUsers = () => {
   let initiatedCount = 0;
-  _.each(scenario1Users, (u, index) => {
+  _.each(users, (u, index) => {
     let sponsor: any = u.sponsor && u.sponsor.userId && users[u.sponsor && u.sponsor.userId];
     if (!u.disabled &&
       !announcementAlreadyInitiated(u) &&
@@ -279,3 +661,156 @@ announceUsers = () => {
   log.info(`initiatedCount: `, initiatedCount);
 }
 announceUsers();
+
+
+let needsAnnouncement;
+needsAnnouncement = (u) => {
+  let sponsor: any = u && u.sponsor && u.sponsor.userId && users[u.sponsor && u.sponsor.userId];
+  return !!u &&
+    !u.disabled &&
+    !announcementAlreadyInitiated(u) &&
+    !!u.wallet &&
+    !!u.wallet.address &&
+    !!sponsor &&
+    !sponsor.disabled &&
+    !!sponsor.wallet &&
+    !!sponsor.wallet.address &&
+    !!sponsor.wallet.announcementTransaction &&
+    !!sponsor.wallet.announcementTransaction.blockNumber &&
+    !!sponsor.wallet.announcementTransaction.hash;
+}
+
+let usersNeedingAnnouncement;
+usersNeedingAnnouncement = _.filter(users, needsAnnouncement);
+
+_.each(usersNeedingAnnouncement, (u) => {
+  firebase.database().ref(`/identityAnnouncementQueue/tasks/${u.userId}`).set({userId: u.userId});
+});
+
+
+let growth;
+growth = (now, a, b) => {
+  let sample = _.filter(users, (u) => {
+    let timeAgo = now - u.createdAt; let period = 60 * 60 * 1000;
+    return (a * period) < timeAgo && timeAgo  <= (b * period) && u.wallet && u.wallet.announcementTransaction;
+  });
+  let nextGen = _.filter(users, (u) => {
+    return u.sponsor &&
+      _.some(sample, (s) => { return s.userId === u.sponsor.userId; });
+  });
+  log.info(`sample size=${_.size(sample)}, next gen size=${_.size(nextGen)}, growth rate=${(_.size(nextGen) - _.size(sample))/ _.size(sample) * 100}`);
+};
+growth(now, 24, 36);
+
+
+
+
+let tasks;
+firebase.database().ref('/phoneAuthQueue/tasks').once('value').then((snapshot) => {
+  tasks = snapshot.val() || {};
+  _.each(tasks, (t, tid) => { t.taskId = tid; });
+});
+
+
+
+
+
+let getTransactionsByAccount = (startBlockNumber, endBlockNumber) => {
+  var count = 0;
+  for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+    var block = web3.eth.getBlock(i, true);
+    if (block != null && block.transactions != null) {
+      block.transactions.forEach( function(e) {
+        if (e.input && e.input.length === 4) {
+          count++;
+        }
+      });
+    }
+  }
+  console.log("Found " + count + " signup transactions");
+}
+getTransactionsByAccount(444000,445000);
+
+
+
+let idTasks;
+let a, b, c, d;
+firebase.database().ref(`/identityAnnouncementQueue/tasks`).once('value', (snapshot) => {
+  idTasks = snapshot.val() || {};
+  _.each(idTasks, (t,tid) => { t.taskId = tid; });
+  log.info('done loading tasks');
+  log.info('grouped by state', _.groupBy(idTasks, '_state'));
+  log.info('grouped by error', _.groupBy(idTasks, '_error_details.error'));
+
+  a = _.filter(idTasks, (t) => {
+    return t._state === 'error' && t._error_details && /announcement-initiated/.test(t._error_details.error);
+  });
+
+});
+
+let now = new Date().valueOf();
+let readyToGo = _.filter(users, (u) => {
+  let sponsor = users[u.sponsor && u.sponsor.userId];
+  if (u.disabled || !u.wallet || !u.wallet.address) {
+    return false;
+  }
+  if ((now - u.wallet.createdAt)/60/1000 <= 2) {
+    return false
+  }
+  let status = getUserStatus()
+  return !u.wallet.announcementTransaction &&
+
+    sponsor && !sponsor.diabled &&
+    sponsor.wallet && sponsor.wallet.announcementTransaction && sponsor.wallet.announcementTransaction.blockNumber;
+});
+_.groupBy(readyToGo, 'registration.status');
+
+_.each(readyToGo, (u) => {
+  firebase.database().ref(`/users/${u.userId}/registration/status`).remove();
+  firebase.database().ref(`/identityAnnouncementQueue/tasks`).push({userId: u.userId});
+});
+
+_.each(c, (t) => {
+  let u = users[t.userId];
+  let ref = userRef(u.userId);
+  let ref1 = ref.child('wallet').child('announcementTransaction');
+  // log.info(`ref1=${ref1.toString()}`);
+  ref1.remove();
+  let ref2 = ref.child('registration').child('status');
+  // log.info(`ref2=${ref2.toString()}`);
+  ref2.remove();
+  let ref3 = firebase.database().ref(`/identityAnnouncementQueue/tasks/${t.taskId}`);
+  // log.info(`ref3=${ref3.toString()}`);
+  ref3.remove();
+});
+
+stop = true
+
+let stop = false;
+let showBlocks = (startingBlock) => {
+  for (let i = 0; i < 50; i++) {
+    let b = startingBlock + i;
+    let numTrans = (web3.eth.getBlock(b, true).transactions || []).length;
+    if (numTrans) {
+      log.info(`block ${b} has ${numTrans} transactions`);
+    }
+  }
+  if (!stop) {
+    _.delay(showBlocks, 250, startingBlock + 50);
+  }
+}
+showBlocks(493300);
+
+_.map(Array.from(Array(100).keys()), (i) => {
+  return (web3.eth.getBlock(b, true).transactions || []).length;
+})
+
+
+_.each(a, (t) => {
+  let u = users[t.userId];
+  let ref3 = firebase.database().ref(`/identityAnnouncementQueue/tasks/${t.taskId}`);
+  ref3.remove();
+});
+
+
+_.filter(users, (u) => { return u.wallet && u.wallet.announcementTransaction && u.wallet.announcementTransaction.blockNumber; })
