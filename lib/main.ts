cf618d41b24e4d4810636b8745a21acb120bc830
John Reitano <jreitano@gmail.com>
2017-01-19 19:08:00 -0800

    change

--- lib/main.ts
+++ lib/main.ts
@@ -3,8 +3,7 @@
 import * as dotenv from 'dotenv';
 import * as log from 'loglevel';
 import * as _ from 'lodash';
-import * as firebase from 'firebase';
-import {ImportProcessor} from './import_processor';
+import {DataManipulator} from './data_manipulator';
 
 if (!process.env.NODE_ENV) {
   dotenv.config(); // if running on local machine, load config vars from .env file, otherwise these come from heroku
@@ -14,21 +13,20 @@ log.setDefaultLevel(process.env.LOG_LEVEL || "info")
 
 log.info(`starting with NODE_ENV ${process.env.NODE_ENV} and FIREBASE_PROJECT_ID ${process.env.FIREBASE_PROJECT_ID}`);
 
-firebase.initializeApp({
-  serviceAccount: `./serviceAccountCredentials.${process.env.FIREBASE_PROJECT_ID}.json`,
+let serviceAccount = require(`../serviceAccountCredentials.${process.env.FIREBASE_PROJECT_ID}.json`);
+let admin = require("firebase-admin");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
-let importProcessor = new ImportProcessor(process.env);
-importProcessor.start().then(() => {
-  log.info("finished importing");
+let dataManipulator = new DataManipulator(process.env, admin.database(), admin.auth());
+dataManipulator.loadUsers().then(() => {
+  log.info("all done!");
   process.exit(0);
-});
+})
 
 process.on('SIGTERM', () => {
   log.info(`Exiting...`);
-  importProcessor.shutdown().then(() => {
-    log.info('finished shutting down');
-    process.exit(0);
-  });
+  process.exit(0);
 });
